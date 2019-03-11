98a6a3f41e7819afda0e46cf088b5606be2d5a19
Misslin Quentin <misslin.quentin@gmail.com>
2019-03-12 00:11:56 +0100

    feat : add material elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Mooni';
+  typesOfShoes: string[] = [
+    'File 1', 'File 2', 'File 3', 'File 4', 'File 5'
+  ];
+  isLinear = false;
+  constructor() {}
+
+  ngOnInit() {
+  }
 }
